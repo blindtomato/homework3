0d718141b6fca063fc51041671c65ddee644c241
Yudin Pavel <yudin.p.s@gmail.com>
2022-11-03 11:35:28 +0700

    feat: add solution of task 21

---
+++ ex21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите координату x точки A");
+int ax = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите координату y точки A");
+int ay = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите координату z точки A");
+int az = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите координату x точки B");
+int bx = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите координату y точки B");
+int by = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите координату z точки B");
+int bz = Convert.ToInt32 (Console.ReadLine());
+
+double distance = Math.Round(Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2)), 2);
+Console.WriteLine($"Расстояние между двумя точками равно {distance}");
