c975114cb0b42a7cbcf9b9cd384b1012444bcfe8
Yudin Pavel <yudin.p.s@gmail.com>
2022-11-03 11:53:31 +0700

    feat: add solution of task 23

---
+++ ex23/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите натуральное число ");
+int number = Convert.ToInt32 (Console.ReadLine());
+int i = 1;
+
+if (number <= 0)
+    Console.WriteLine("Вы ввели некорректное число");
+else 
+    for (i = 1; i <= number; i++)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+    }
